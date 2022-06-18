cd7d73ac7c3edf0703c04f6006cb8567ade780c8
moulin-louis <louis.moulin@outlook.fr>
2022-06-18 21:56:14 +0200

    add libft ft_printf

---
+++ ft_printf/srcs/ft_putnbr_base_printf.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base_printf.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loumouli <loumouli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 12:28:35 by loumouli          #+#    #+#             */
+/*   Updated: 2022/05/17 11:08:08 by loumouli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ft_printf.h"
+
+static	int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+static	int	ft_nbr_equal_zero(void)
+{
+	ft_putchar_printf('0');
+	return (1);
+}
+
+int	ft_putnbr_base_printf(int nbr, char *base)
+{
+	long	nl;
+	int		i;
+	int		b[12];
+	int		count;
+	int		size_len;
+
+	nl = nbr;
+	i = 0;
+	count = 0;
+	size_len = ft_strlen(base);
+	if (nl < 0)
+		nl = (unsigned int)nl;
+	if (nl == 0)
+		return (ft_nbr_equal_zero());
+	while (nl > 0)
+	{
+		b[i++] = nl % size_len;
+		nl = nl / size_len;
+	}
+	while (--i >= 0)
+		count += ft_putchar_printf(base[b[i]]);
+	return (count);
+}
---
+++ ft_printf/srcs/ft_putstr_printf.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_printf.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loumouli <loumouli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 15:46:43 by loumouli          #+#    #+#             */
+/*   Updated: 2022/05/17 11:14:42 by loumouli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ft_printf.h"
+
+int	ft_putstr_printf(char *s)
+{
+	int	x;
+
+	x = 0;
+	if (s == NULL)
+		return (ft_putstr_printf("(null)"));
+	while (s[x])
+		x += ft_putchar_printf(s[x]);
+	return (x);
+}
