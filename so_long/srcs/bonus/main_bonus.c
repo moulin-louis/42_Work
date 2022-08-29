ac47d2e4ba1bae5bb4b45fd9b283be92cd59d145
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2022-08-29 14:54:16 +0200

    update so_long for the last time i hope ffs

--- so_long/srcs/bonus/main_bonus.c
+++ so_long/srcs/bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: loumouli <loumouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 15:48:07 by loumouli          #+#    #+#             */
-/*   Updated: 2022/07/25 15:34:50 by loumouli         ###   ########.fr       */
+/*   Updated: 2022/08/29 14:25:11 by loumouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,29 @@ int	find_nbr_coll(t_data *data)
 	return (1);
 }
 
+int	check_char_map(t_data *data)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	while (data->map[x])
+	{
+		y = 0;
+		while (data->map[x][y])
+		{
+			if (data->map[x][y] != 'P' && data->map[x][y] != '0'
+			&& data->map[x][y] != '1' && data->map[x][y] != 'E'
+			&& data->map[x][y] != 'C' && data->map[x][y] != 'N')
+				return (destroy_anything(data), -1);
+			y++;
+		}
+		x++;
+	}
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	t_data	data;
@@ -87,10 +110,12 @@ int	main(int ac, char **av)
 	nullify_void_ptr(&data);
 	if (setup_map(&data, av[1]) == -1)
 		return (-1);
-	if (setup_init(&data) == 0)
-		return (-3);
 	if (find_nbr_coll(&data) == -1)
 		return (ft_putstr_fd("Error\nProbleme exit/player\n", 1), -4);
+	if (check_char_map(&data) == -1)
+		return (ft_putstr_fd("Error\nInvalid char\n", 1), 1);
+	if (setup_init(&data) == 0)
+		return (-3);
 	push_map(&data);
 	setup_hook(&data);
 	mlx_loop(data.init);
