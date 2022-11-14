4dda9495f739eeea5dd2c39b99342847d1d46b49
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2022-11-14 15:21:01 +0100

    start philo + add docs pthread fn

---
+++ philosophers/inc/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loumouli <loumouli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 14:49:01 by loumouli          #+#    #+#             */
+/*   Updated: 2022/11/14 14:50:43 by loumouli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
---
+++ philosophers/objs/main.d
@@ -0,0 +1,1 @@
+objs/main.o: src/main.c inc/philo.h
